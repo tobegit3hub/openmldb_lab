ee63e3e6d886145a8e115ddb5dcf24c99be7817f
tobe <tobeg3oogle@gmail.com>
2022-01-28 18:23:21 +0800

    Initialize frontend vue app

---
+++ vueapp/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- 
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	-->
+	<Console />
+  </div>
+</template>
+
+<script>
+import Console from './components/Console.vue'
+
+export default {
+  name: 'App',
+  components: {
+	Console
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueapp/src/components/Console.vue
@@ -0,0 +1,57 @@
+<template>
+
+<div class="console">
+  <div class="db_list">
+    <h2>Databases</h2>
+    <ul>
+      <li>db1</li>
+      <li>db2</li>
+      <li>db3</li>
+    </ul>
+  </div>
+
+  <div class="table_list">
+    <h2>Tables</h2>
+    <ul>
+      <li>t1</li>
+      <li>t2</li>
+      <li>t3</li>
+    </ul>
+  </div>
+  
+  <div class="sql_playground">
+  <input />
+    <div class="sql_result">
+
+    </div>
+  </div>
+</div>
+
+</template>
+
+<script>
+export default {
+  name: 'Console',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
