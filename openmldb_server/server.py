4c85c8b4d83964b939b94ecf719e20d70bca4eb1
tobe <tobeg3oogle@gmail.com>
2022-01-29 10:56:14 +0800

    Add show tables in backend

--- openmldb_server/server.py
+++ openmldb_server/server.py
@@ -1,15 +1,36 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS, cross_origin
+import openmldb.dbapi
 
 app = Flask(__name__,
             template_folder="./dist/",
             static_folder="./dist",
             static_url_path="")
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+db = openmldb.dbapi.connect("db_test", "127.0.0.1:6181", "/onebox")
+cursor = db.cursor()
 
 @app.route('/')
+@cross_origin()
 def index():
     return render_template("index.html")
+
+@app.route('/api/dbs')
+@cross_origin()
+def get_dbs():
+    # TODO: Not supported yet
+    return render_template("index.html")
+
+@app.route('/api/tables')
+@cross_origin()
+def get_tables():
+    tables = cursor.get_all_tables()
+    #import ipdb;ipdb.set_trace()
+    return jsonify(tables=tables)
 
 def main():
   app.run(host="0.0.0.0")
