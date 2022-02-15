e19443df8d31ab884273d0f6015f27003f1df6ae
tobe <tobeg3oogle@gmail.com>
2022-02-15 17:25:15 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 set -xe
 
-# Install Python libraries
-pip install -r ./requirements.txt
-
 # Install frontend
+npm install -g @vue/cli
 cd ./vueapp
+npm install
 npm run build
 cd ../
 
 # Install backend
+pip install -r ./requirements.txt
 python ./setup.py install
